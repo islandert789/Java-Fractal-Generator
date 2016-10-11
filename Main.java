5ee5408f531eabd0a2d40ce388f3fe6435ca781b
islandert789 <islandert789@yahoo.com>
2016-10-11 14:26:10 -0700

    Added command line support

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
-//import java.awt.*;
+//import java.io.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        double[] base = {90};
-        Fractal f = new Fractal(base,10,true).setOptimalAngle().display();
+        for (String s : args) {
+            //System.out.println(s);
+        }
+        String turnsString = new String(args[0]);
+        String turnsStringWithoutBrackets = turnsString.substring(1,turnsString.length()-1);
+        String[] splitTurnsString = turnsStringWithoutBrackets.split(",");
+        double[] base = new double[splitTurnsString.length];
+        for (int i = 0; i < splitTurnsString.length; i++) {
+            base[i] = Double.parseDouble(splitTurnsString[i]);
+        }
+        int iteration = Integer.parseInt(args[1]);
+        boolean mirrored = Boolean.parseBoolean(args[2]);
+        Fractal f = new Fractal(base,iteration,mirrored).setOptimalAngle().display();
     }
 }
